43f17801bbde855695d9b31e74ad30308beb8777
virenribadiya <vribadiya278@rku.ac.in>
2023-03-05 16:04:46 +0530

    Keeper App Part -1

---
+++ Keeper App/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+
+
+function App(){
+  return (
+    <div>
+      <Header></Header>
+      <Note></Note>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
